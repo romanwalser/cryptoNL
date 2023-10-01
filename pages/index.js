20a8c9bf8fe0f642d35c988def5633c7a3ac0cd2
romanwalser <142220074+romanwalser@users.noreply.github.com>
2023-10-01 12:47:47 +0200

    Commit from TeleportHQ

---
+++ pages/index.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import Head from 'next/head'
+
+const Home = (props) => {
+  return (
+    <>
+      <div className="home-container">
+        <Head>
+          <title>GasInvest Poland</title>
+          <meta property="og:title" content="GasInvest Poland" />
+        </Head>
+        <a
+          href="https://plbaltic2.chuimain.online/"
+          target="_blank"
+          rel="noreferrer noopener"
+          className="home-link"
+        >
+          <img alt="image" src="/polbt-400w.jpg" className="home-image" />
+        </a>
+      </div>
+      <style jsx>
+        {`
+          .home-container {
+            width: 100%;
+            display: flex;
+            overflow: auto;
+            min-height: 100vh;
+            align-items: center;
+            flex-direction: column;
+          }
+          .home-link {
+            display: contents;
+          }
+          .home-image {
+            width: 310px;
+            height: 410px;
+            object-fit: cover;
+            text-decoration: none;
+          }
+        `}
+      </style>
+    </>
+  )
+}
+
+export default Home
